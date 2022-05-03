37915ac2d5a8d08df46c9f1ed72352409715d007
Sahil <hanss9545@gmail.com>
2022-05-03 23:59:01 +0530

    first commit
    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let mobile_menubar = document.querySelector(".mobile-menubar");
+let close_mobileMenu = document.querySelector(".close_mobileMenu ");
+mobile_menubar.addEventListener("click", () => {
+  document
+    .querySelector(".header nav .menu")
+    .classList.add("active-mobileMenu");
+});
+
+close_mobileMenu.addEventListener("click", () => {
+  document
+    .querySelector(".header nav .menu")
+    .classList.remove("active-mobileMenu");
+});
