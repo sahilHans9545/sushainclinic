3876b1f29f8393bbee6e4f783917a4cb96bc4782
Sahil <hanss9545@gmail.com>
2022-05-05 09:55:08 +0530

    sliders and responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,36 @@ close_mobileMenu.addEventListener("click", () => {
     .querySelector(".header nav .menu")
     .classList.remove("active-mobileMenu");
 });
+
+// ---- patients speaks about us Slider ----
+
+let patient_slide_video = document.querySelector(
+  ".what-patient-speak .slider .slide video"
+);
+
+let patient_slide_left = document.querySelector(
+  ".patient-slider-controls .leftBtn"
+);
+
+let patient_slide_right = document.querySelector(
+  ".patient-slider-controls .rightBtn"
+);
+
+let patient_videos = ["patient-speak.mp4", "big_buck_bunny_720p_5mb.mp4"];
+let patient_slide = 0;
+
+patient_slide_video.src = `./assets/videos/${patient_videos[patient_slide]}`;
+
+patient_slide_left.addEventListener("click", () => {
+  if (patient_slide > 0) {
+    patient_slide--;
+    patient_slide_video.src = `./assets/videos/${patient_videos[patient_slide]}`;
+  }
+});
+
+patient_slide_right.addEventListener("click", () => {
+  if (patient_slide < patient_videos.length - 1) {
+    patient_slide++;
+    patient_slide_video.src = `./assets/videos/${patient_videos[patient_slide]}`;
+  }
+});
