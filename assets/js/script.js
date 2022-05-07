b758bbb6acdd22cbbae94ec3dba5fafa6cd3a79e
Sahil <hanss9545@gmail.com>
2022-05-07 14:28:15 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,26 @@ close_mobileMenu.addEventListener("click", () => {
     .querySelector(".header nav .menu")
     .classList.remove("active-mobileMenu");
 });
+
+// Multiple videos will not play at a time in what patient speaks about us
+var videos = document.querySelectorAll(".what-patient-speak video");
+for (var i = 0; i < videos.length; i++)
+  videos[i].addEventListener(
+    "play",
+    function () {
+      pauseAll(this);
+    },
+    true
+  );
+
+function pauseAll(elem) {
+  for (var i = 0; i < videos.length; i++) {
+    //Is this the one we want to play?
+    if (videos[i] == elem) continue;
+    //Have we already played it && is it already paused?
+    if (videos[i].played.length > 0 && !videos[i].paused) {
+      // Then pause it now
+      videos[i].pause();
+    }
+  }
+}
