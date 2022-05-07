a251b5e4dc7c08a2cc9587cd6144325829846c4b
Sahil <hanss9545@gmail.com>
2022-05-08 02:04:00 +0530

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,10 @@ let video_popup = document.querySelector(".video-popup");
 let vp_video = video_popup.querySelector("video");
 console.log(vp_video);
 let close_video_popup = document.querySelector(".close_video_popup");
-let patient_video_play = document.querySelectorAll(
-  ".what-patient-speak .video-play"
-);
-console.log(patient_video_play);
+let video_play = document.querySelectorAll(".video-play");
+console.log(video_play);
 
-patient_video_play.forEach((video_play) => {
+video_play.forEach((video_play) => {
   video_play.addEventListener("click", () => {
     video_popup.classList.add("active-video-popup");
     vp_video.src = video_play.getAttribute("video_url");
@@ -32,4 +30,21 @@ patient_video_play.forEach((video_play) => {
 close_video_popup.addEventListener("click", () => {
   video_popup.classList.remove("active-video-popup");
   vp_video.pause();
+});
+
+// veteren-section
+let veterenMoreText = document.querySelectorAll(".veteren-content .hide-text");
+let moreLessBtn = document.querySelectorAll(".moreless-button");
+
+moreLessBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("hello");
+    let show_hide_text = btn.previousElementSibling.querySelector(".text");
+    show_hide_text.classList.toggle("hide-text");
+    if (show_hide_text.classList.contains("hide-text")) {
+      btn.textContent = "Read more";
+    } else {
+      btn.textContent = "Read Less";
+    }
+  });
 });
